114cd07c830bf62aac80a5f3b2ee9c2e89b1beaf
Aaron Sheppard <nurah@wolfo.tech>
2023-01-08 22:13:42 +0000

    SauceNAO: Handle when we are out of API calls

--- src/functions/saucenao.rs
+++ src/functions/saucenao.rs
@@ -4,7 +4,6 @@ use poise::{
     serenity_prelude::{ButtonStyle, CreateComponents, InteractionResponseType},
     ReplyHandle
 };
-
 use std::time::Duration;
 
 async fn reverse_client(url: &String, api_key: &String) -> Result<SauceNAO, reqwest::Error> {
@@ -36,7 +35,10 @@ async fn reverse_client(url: &String, api_key: &String) -> Result<SauceNAO, reqw
     match response_resolved {
         Ok(saucenao) => Ok(saucenao),
         Err(err) => {
-            println!("Furaffinity: Failed to decode to JSON object, reason following... {err}");
+            println!("Saucenao: {err}\nRequest URL: {url}");
+            if let Some(status_code) = err.status() {
+                println!("Status Code: {status_code}");
+            };
             Err(err)
         }
     }
@@ -46,15 +48,31 @@ pub async fn interactive_response<'a>(ctx: Context<'a>, url: String, api_key: &S
     let mut cursor = 0;
     let sauce = match reverse_client(&url, api_key).await {
         Ok(saucenao) => saucenao,
-        Err(err) => return ctx.say(format!("API Error: {err}")).await
+        Err(err) => {
+            if let Some(status_code) = err.status() {
+                return ctx.say(format!("This API is limited to 100 requests a day because Nurah is not gonna spend 6$ a month for this... If you *really* want more requests, consider donating.\nStatus Code: {status_code}")).await;
+            } else {
+                return ctx.say(format!("API Error: {err}")).await
+            };
+
+        }
     };
 
-    let result = match sauce.results.get(cursor) {
-        Some(result) => result,
+    if sauce.header.status == -2 {
+        return ctx.say("This API is limited to 100 requests a day because Nurah is not gonna spend 6$ a month for this... If you *really* want more requests, consider donating.".to_string()).await;
+    };
+
+    let results = match &sauce.results {
+        Some(results) => {
+            match results.get(cursor) {
+                Some(result) => result,
+                None => return ctx.say("Did not find anything ;)").await
+            }
+        },
         None => return ctx.say("Did not find anything ;)").await
     };
 
-    let reply_handle = match result.data.ext_urls.first() {
+    let reply_handle = match results.data.ext_urls.first() {
         Some(result) => {
             ctx.send(|builder| {
                 builder.content(result);
@@ -85,40 +103,47 @@ pub async fn interactive_response<'a>(ctx: Context<'a>, url: String, api_key: &S
         if interaction.data.custom_id.contains("prev") {
             cursor -= 1;
 
-            let result = match sauce.results.get(cursor) {
-                Some(result) => result,
-                None => return ctx.say("Did not find anything ;)").await
+            if let Some(results) = &sauce.results {
+                if let Some(result) = results.get(cursor) {
+                    reply_handle
+                    .edit(ctx, |builder| {
+                        builder.content(result.data.ext_urls.first().unwrap());
+                        builder.components(|c| {
+                            *c = components(&sauce, &cursor, false, false);
+                            c
+                        });
+                        builder
+                    })
+                    .await?
+                } else {
+                    reply_handle.edit(ctx, |builder|builder.content("Did not find anything ;)")).await?
+                }
+            } else {
+                reply_handle.edit(ctx, |builder|builder.content("Did not find anything ;)")).await?
             };
-
-            reply_handle
-                .edit(ctx, |builder| {
-                    builder.content(result.data.ext_urls.first().unwrap());
-                    builder.components(|c| {
-                        *c = components(&sauce, &cursor, false, false);
-                        c
-                    });
-                    builder
-                })
-                .await?;
         }
+
         if interaction.data.custom_id.contains("next") {
             cursor += 1;
 
-            let result = match sauce.results.get(cursor) {
-                Some(result) => result,
-                None => return ctx.say("Did not find anything ;)").await
+            if let Some(results) = &sauce.results {
+                if let Some(result) = results.get(cursor) {
+                    reply_handle
+                    .edit(ctx, |builder| {
+                        builder.content(result.data.ext_urls.first().unwrap());
+                        builder.components(|c| {
+                            *c = components(&sauce, &cursor, false, false);
+                            c
+                        });
+                        builder
+                    })
+                    .await?
+                } else {
+                    reply_handle.edit(ctx, |builder|builder.content("Did not find anything ;)")).await?
+                }
+            } else {
+                reply_handle.edit(ctx, |builder|builder.content("Did not find anything ;)")).await?
             };
-
-            reply_handle
-                .edit(ctx, |builder| {
-                    builder.content(result.data.ext_urls.first().unwrap());
-                    builder.components(|c| {
-                        *c = components(&sauce, &cursor, false, false);
-                        c
-                    });
-                    builder
-                })
-                .await?;
         }
     }
 
@@ -146,7 +171,7 @@ fn components(saucenao: &SauceNAO, cursor: &usize, disable_left: bool, disable_r
         });
 
         row.create_button(|button| {
-            let disabled = if disable_right { true } else { cursor >= &saucenao.results.len() };
+            let disabled = if disable_right { true } else { cursor >= &saucenao.results.clone().unwrap().len() };
 
             button.custom_id("next").label("Next Source").style(ButtonStyle::Primary).disabled(disabled);
             button
--- src/structs/saucenao.rs
+++ src/structs/saucenao.rs
@@ -2,25 +2,26 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Header {
-    pub user_id: String,
-    pub account_type: String,
-    pub short_limit: String,
-    pub long_limit: String,
-    pub long_remaining: u64,
-    pub short_remaining: u64,
-    pub status: u64,
-    pub results_requested: String,
-    pub index: HashMap<String, Index>,
-    pub search_depth: String,
-    pub minimum_similarity: f64,
-    pub query_image_display: String,
-    pub query_image: String,
-    pub results_returned: u64
+    pub user_id: Option<String>,
+    pub account_type: Option<String>,
+    pub short_limit: Option<String>,
+    pub long_limit: Option<String>,
+    pub long_remaining: Option<u64>,
+    pub short_remaining: Option<u64>,
+    pub status: i64,
+    pub results_requested: Option<String>,
+    pub index: Option<HashMap<String, Index>>,
+    pub search_depth: Option<String>,
+    pub minimum_similarity: Option<f64>,
+    pub query_image_display: Option<String>,
+    pub query_image: Option<String>,
+    pub results_returned: Option<u64>,
+    pub message: Option<String>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Index {
     pub status: u64,
     pub parent_id: u64,
@@ -28,7 +29,7 @@ pub struct Index {
     pub results: Option<u64>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct HeaderResult {
     pub similarity: String,
     pub thumbnail: String,
@@ -38,7 +39,7 @@ pub struct HeaderResult {
     pub hidden: u64
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Data {
     pub ext_urls: Vec<String>,
     pub title: Option<String>,
@@ -47,14 +48,14 @@ pub struct Data {
     pub member_id: Option<u64>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Results {
     pub header: HeaderResult,
     pub data: Data
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct SauceNAO {
     pub header: Header,
-    pub results: Vec<Results>
+    pub results: Option<Vec<Results>>
 }
