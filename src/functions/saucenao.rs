d8adfdf1dc146ef3368ec946c0f0209a81dc2fe6
Aaron Sheppard <nurah@wolfo.tech>
2023-01-10 03:16:44 +0000

    Saucenao: More sources

--- src/functions/saucenao.rs
+++ src/functions/saucenao.rs
@@ -14,12 +14,15 @@ async fn reverse_client(url: &String, api_key: &String) -> Result<SauceNAO, reqw
         .query(&[
             ("api_key", api_key),
             // ("testmode","1"),
-            ("output_type", &"2".to_string()),
-            ("dbs[]", &"29".to_string()),
-            ("dbs[]", &"39".to_string()),
-            ("dbs[]", &"40".to_string()),
-            ("dbs[]", &"41".to_string()),
-            ("dbs[]", &"42".to_string()),
+            ("output_type", &"2".to_string()), // https://saucenao.com/tools/examples/api/index_details.txt
+            ("dbs[]", &"25".to_string()), // Gelbooru
+            ("dbs[]", &"29".to_string()), // E621
+            ("dbs[]", &"34".to_string()), // DeviantArt
+            ("dbs[]", &"39".to_string()), // ArtStation
+            ("dbs[]", &"40".to_string()), // FurAffinity
+            ("dbs[]", &"41".to_string()), // Twitter
+            ("dbs[]", &"42".to_string()), // Furry Network
+            ("dbs[]", &"43".to_string()), // Kemono
             ("url", url)
         ])
         .send()
