3cc7f2710326044724cf0a6e5d4bf25b909500dc
Aaron Sheppard <nurah@wolfo.tech>
2023-01-15 04:20:37 +0000

    FIX: User context menu for quotes command

--- src/commands/quote/mod.rs
+++ src/commands/quote/mod.rs
@@ -1,17 +1,41 @@
+use poise::serenity_prelude::Message;
+
 use crate::{
-    commands::quote::{quote_add::addquote, quote_get::get, quote_user::user},
-    Command, Context, Error
+    commands::quote::{quote_get::get, quote_user::user},
+    Command, Context, Error, functions::guild_accent_colour::guild_accent_colour, data::quotes::{Quote, Quotes}, constants::QUOTES_FILE_PATH
 };
 
-mod quote_add;
 mod quote_get;
 mod quote_user;
 mod story;
 
 /// Get some information on things, like guilds and users.
-#[poise::command(slash_command, category = "Guild", subcommands("addquote", "get", "user"))]
-pub async fn quote(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("This command only has subcommands I'm afraid :)").await?;
+#[poise::command(context_menu_command = "Save this quote", slash_command, category = "Guild", subcommands("get", "user"))]
+pub async fn quote(
+    ctx: Context<'_>,
+    #[description = "The quote which you wish to add to the database"] message: Message
+) -> Result<(), Error> {
+    let accent_colour = ctx.data().config.read().await.accent_colour;
+    let quotes = &ctx.data().quotes.read().await.quotes;
+    let mut new_quote = vec![Quote {
+        user_id: *message.author.id.as_u64(),
+        quote: String::from(&message.content)
+    }];
+
+    ctx.send(|b| {
+        b.embed(|b| {
+            b.author(|a| a.name(&message.author.name).icon_url(&message.author.face()))
+                .title("Quote Added!")
+                .description(&message.content)
+                .color(guild_accent_colour(accent_colour, ctx.guild()))
+                .footer(|f| f.text(format!("Quote ID: {}", quotes.len())))
+        })
+    })
+    .await?;
+    let quotes = &mut ctx.data().quotes.write().await;
+    quotes.quotes.append(&mut new_quote);
+    Quotes::write(quotes, QUOTES_FILE_PATH).await;
+
     Ok(())
 }
 
--- src/commands/quote/quote_user.rs
+++ src/commands/quote/quote_user.rs
@@ -3,9 +3,7 @@ use rand::Rng;
 
 use crate::{functions::sendquote::send_quote, Context, Error};
 
-/// Get random shit a user has said ;)
-#[poise::command(slash_command, category = "Quotes")]
-pub async fn user(ctx: Context<'_>, #[description = "User to get a random quote from"] user: User) -> Result<(), Error> {
+async fn get_user_quotes(ctx: Context<'_>, user: User) -> Result<(), Error> {
     let quotes = &ctx.data().quotes.read().await.quotes;
 
     let quotes_by_user = quotes
@@ -31,33 +29,16 @@ pub async fn user(ctx: Context<'_>, #[description = "User to get a random quote 
     Ok(())
 }
 
-/// Get random shit a user has said - User context edition
-#[poise::command(context_menu_command = "User quotes", slash_command, category = "Quotes")]
-pub async fn quote_user_context(
-    ctx: Context<'_>,
-    #[description = "User to get a random quote from"] user: User
-) -> Result<(), Error> {
-    let quotes = &ctx.data().quotes.read().await.quotes;
+/// Get random shit a user has said ;)
+#[poise::command(slash_command, category = "Quotes")]
+pub async fn user(ctx: Context<'_>, #[description = "User to get a random quote from"] user: User) -> Result<(), Error> {
+    get_user_quotes(ctx, user).await?;
+    Ok(())
+}
 
-    let quotes_by_user = quotes
-        .iter()
-        .enumerate()
-        .filter(|(_, quote)| quote.user_id == user.id.0)
-        .collect::<Vec<_>>();
-    if quotes_by_user.is_empty() {
-        ctx.say("No quotes found for that user >:c\nTell them to shitpost more")
-            .await?;
-        return Ok(());
-    }
-
-    let random_number = rand::thread_rng().gen_range(0..quotes_by_user.len());
-
-    // No quote specified, so lets get a random one instead!
-    if let Some(quote) = quotes_by_user.get(random_number) {
-        send_quote(ctx, quote.1, Some(random_number)).await?;
-    } else {
-        ctx.say("Failed to find a quote").await?;
-    }
-
+/// Get random shit a user has said ;)
+#[poise::command(context_menu_command = "Quotes by this user", category = "Quotes")]
+pub async fn quote_user_context(ctx: Context<'_>, #[description = "User to get a random quote from"] user: User) -> Result<(), Error> {
+    get_user_quotes(ctx, user).await?;
     Ok(())
 }
