2f613b96e0743de2ead6ef0bab3c8741d581d8a3
Aaron Sheppard <nurah@wolfo.tech>
2023-01-22 16:37:31 +0000

    Heck: Split into functions + Better error handling

--- lib/luro_commands/src/testing/heck.rs
+++ lib/luro_commands/src/testing/heck.rs
@@ -9,6 +9,41 @@ use poise::{
 };
 use rand::{seq::SliceRandom, Rng};
 
+/// Open the database as writable in case we need to reload the hecks
+async fn check_hecks_are_present(data: &Data) -> Result<(), Error> {
+    let heck_db = &mut data.heck.write().await;
+
+    if heck_db.available_heck_ids.is_empty() {
+        heck_db.reload_ids();
+    };
+
+    Ok(())
+}
+
+/// Open the database as writeable and remove a heck from it, returning the heck removed
+async fn get_heck(data: &Data, heck_id: Option<usize>) -> Result<(Heck, usize), String> {
+    let heck_db = &mut data.heck.write().await;
+
+    let heck = match heck_id {
+        Some(heck_id) => match heck_db.hecks.get(heck_id) {
+            Some(heck) => Ok((heck.to_owned(), heck_id)),
+            None => Err("Failed to find that heck ID".to_string())
+        },
+        None => {
+            // Get a random heck
+            let random_number = rand::thread_rng().gen_range(0..heck_db.available_heck_ids.len());
+            let random_heck_id = heck_db.available_heck_ids.remove(random_number);
+
+            match heck_db.hecks.get(random_heck_id) {
+                Some(heck) => Ok((heck.to_owned(), random_number)),
+                None => Err("Failed to get a random heck".to_string())
+            }
+        }
+    };
+
+    return heck;
+}
+
 #[derive(Debug, Modal)]
 #[name = "Add your heck"] // Struct name by default
 struct AddHeck {
@@ -34,34 +69,35 @@ pub async fn heck(
     plaintext: bool,
     #[description = "Get a particular heck. Random heck returned if not found"] heck_id: Option<usize>
 ) -> Result<(), Error> {
-    let heck_db = &mut ctx.data().heck.write().await;
-
-    if heck_db.available_heck_ids.is_empty() {
-        heck_db.reload_ids();
-    };
-
-    let mut heck;
+    // Make sure that the hecks have not been fully drained from the database, if so, reload them. This opens the DB as writable for the life of the function
+    check_hecks_are_present(ctx.data).await?;
 
     if new_heck {
-        let heckid;
-        heck = if let Some(new_heck) = AddHeck::execute(ctx).await? {
-            heckid = heck_db.hecks.len() + 1;
-            Heck {
+        let heck_db = &mut ctx.data.heck.write().await;
+        let new_heck = match AddHeck::execute(ctx).await{
+            Ok(heck) => heck,
+            Err(err) => {
+                ctx.say(format!("The model had an error: {err}")).await?;
+                return Ok(());
+            }
+        };
+
+        let (mut heck, heckid) = if let Some(new_heck) = new_heck {
+            (Heck {
                 heck_message: new_heck.heck,
                 author_id: ctx.author().id.0
-            }
+            }, heck_db.hecks.len())
         } else {
-            ctx.say("Your heck was not present. Make sure you include `<user>`!\n\nFor example: `<author> topped <user>!` You can use `\\n` for a newline")
+            ctx.say("Your heck was not present. Make sure you include `<user>`!\n\nFor example: `<author> topped <user>!` You can hold shift and press enter for a newline.")
                 .await?;
             return Ok(());
         };
 
-        // First Check: If an owner is running the command, don't check to make sure the message contains both <user> and <author>.
-        // This is so you can have custom messages, and its implied the owners know what they are doing...
-        // Second Check: Make sure the input contains both <user> and <author>
+        // First Check: If an owner is running the command, don't check to make sure the message contains either <user> or <author>.
+        // This is so you can have fully custom messages, and its implied the owners knows what they are doing...
+        // Second Check: Make sure the input contains at least <user>
         if ctx.framework().options.owners.contains(&ctx.author().id) || heck.heck_message.contains("<user>") {
-            // We want to write the raw string to disk, so we update heck again AFTER it has been written.
-            // Add our new heck
+            // We want to write the raw string to disk, so we update the heck again AFTER it has been written.
             heck_db.hecks.append(&mut vec![heck.clone()]);
             Hecks::write(&heck_db, HECK_FILE_PATH).await; // Save our new heck to the database, unformatted.
                                                           // Now regenerate the heck IDs
@@ -73,37 +109,11 @@ pub async fn heck(
         } else {
             // Format not allowed!
             ctx.say(format!(
-                "Your heck was `{}` but the format was wrong. Make sure you include `<user>`!\n\nFor example: `<author> topped <user>!`",
+                "Your heck was `{}` but the format was wrong. Make sure you include at least `<user>`!\n\nFor example: `<author> topped <user>!`",
                 heck.heck_message
             ))
             .await?;
             return Ok(()); // We can exit the function now
-        }
-    };
-
-    // Get a specific heck
-    if let Some(heck_id) = heck_id {
-        match heck_db.hecks.get(heck_id) {
-            Some(h) => {
-                heck = format_heck(&h, ctx.author(), &user).await;
-                send_heck(heck.clone(), &heck_id, plaintext, ctx, user).await?;
-                return Ok(());
-            }
-            None => {
-                ctx.say("Failed to find that heck ID").await?;
-            }
-        };
-    }
-
-    // Get a random heck
-    let random_number = rand::thread_rng().gen_range(0..heck_db.available_heck_ids.len());
-    let random_heck_id = heck_db.available_heck_ids.remove(random_number);
-
-    heck = match heck_db.hecks.get(random_heck_id) {
-        Some(heck_id) => heck_id.to_owned(),
-        None => {
-            ctx.say("Failed to get a random heck").await?;
-            return Ok(());
         }
     };
 
@@ -115,8 +125,16 @@ pub async fn heck(
         }
     };
 
-    heck = format_heck(&heck, ctx.author(), &user).await;
-    send_heck(heck, &random_heck_id, plaintext, ctx, user).await?;
+    let heck = match get_heck(ctx.data, heck_id).await {
+        Ok(heck) => heck,
+        Err(err) => {
+            ctx.say(format!("Failed to get heck: {err}")).await?;
+            return Ok(());
+        }
+    };
+
+    let formatted_heck = format_heck(&heck.0, ctx.author(), &user).await;
+    send_heck(formatted_heck, &heck.1, plaintext, ctx, user).await?;
 
     Ok(())
 }
