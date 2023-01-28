99e009847e415beb005ac3dc2d67d8390f9667db
Aaron Sheppard <nurah@wolfo.tech>
2023-01-28 13:02:58 +0000

    Delete Bot Message: Add error handling

--- lib/luro_commands/src/commands/context_commands/delete_bot_message.rs
+++ lib/luro_commands/src/commands/context_commands/delete_bot_message.rs
@@ -4,8 +4,10 @@ use poise::serenity_prelude::Message;
 /// Deletes a particular message sent by the bot.
 #[poise::command(context_menu_command = "Delete message by bot", category = "Luro", ephemeral)]
 pub async fn delete_bot_message(ctx: Context<'_>, #[description = "Message to be deleted"] msg: Message) -> Result<(), Error> {
-    msg.delete(ctx).await?;
-    ctx.say("Deleted!").await?;
-
+    match msg.delete(ctx).await {
+        Ok(_) => ctx.say("Deleted!").await?,
+        Err(err) => ctx.say(format!("Failed to delete that message, probably because I don't have permissions. The error was: {err}")).await?,
+    };
+    
     Ok(())
 }
