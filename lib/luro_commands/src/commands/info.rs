d25f901e2113b0e86872dff7ff72604dc9a18c7d
Aaron Sheppard <nurah@wolfo.tech>
2023-01-28 09:27:12 +0000

    feat: Heck info command

--- lib/luro_commands/src/commands/info.rs
+++ lib/luro_commands/src/commands/info.rs
@@ -2,12 +2,14 @@ use luro_core::{Context, Error};
 
 use crate::commands::info::channel::channel;
 use crate::commands::info::guild::guild;
+use crate::commands::info::hecks::hecks;
 use crate::commands::info::message::message;
 use crate::commands::info::role::role;
 use crate::commands::info::user::user;
 
 mod channel;
 mod guild;
+mod hecks;
 mod message;
 mod role;
 mod user;
@@ -16,7 +18,7 @@ mod user;
 #[poise::command(
     slash_command,
     category = "Guild",
-    subcommands("user", "guild", "channel", "message", "role")
+    subcommands("user", "guild", "channel", "message", "role", "hecks")
 )]
 pub async fn info(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("This command only has subcommands I'm afraid :)").await?;
