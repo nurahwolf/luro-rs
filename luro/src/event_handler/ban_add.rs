d8f578703b788e8cd96aed278a51f592ff40a315
Aaron Sheppard <nurah@wolfo.tech>
2023-07-20 19:10:47 +0100

    TWEAK: Better ban message

--- luro/src/event_handler/ban_add.rs
+++ luro/src/event_handler/ban_add.rs
@@ -2,18 +2,13 @@ use std::sync::Arc;
 
 use tracing::info;
 use twilight_model::gateway::payload::incoming::BanAdd;
-use twilight_util::builder::embed::EmbedFieldBuilder;
+use twilight_util::builder::embed::{EmbedFieldBuilder, ImageSource};
 
-use crate::{functions::default_embed, models::LuroFramework};
+use crate::{functions::{default_embed, get_user_avatar}, models::LuroFramework};
 
 impl LuroFramework {
     pub async fn ban_add_listener(self: Arc<Self>, ban: BanAdd) -> anyhow::Result<()> {
         info!("User {} banned from guild {}", ban.user.name, ban.guild_id);
-
-        // Exit early if it was the bot that performed the ban
-        if ban.user.id == self.twilight_client.current_user().await?.model().await?.id {
-            return Ok(());
-        }
 
         let guild_db = self.guild_data.read().clone();
         let guild_settings = match guild_db.get(&ban.guild_id) {
@@ -25,19 +20,19 @@ impl LuroFramework {
             let (banned_user_id, banned_user_name) = (ban.user.id, ban.user.name);
             let resolved_ban = self.twilight_client.ban(ban.guild_id, ban.user.id).await?.model().await?;
             let guild = self.twilight_client.guild(ban.guild_id).await?.model().await?;
+            let banned_avatar = get_user_avatar(&resolved_ban.user);
 
             let mut embed = default_embed(&self, Some(ban.guild_id))
                 .title(format!("Banned from {}", guild.name))
-                .field(EmbedFieldBuilder::new("Guild ID", guild.id.to_string()).inline());
+                .thumbnail(ImageSource::url(banned_avatar)?);
 
-            match resolved_ban.reason {
-                    Some(reason) =>                     embed = embed.description(format!(
-                        "**User:** <@{banned_user_id}> - {banned_user_name}\n**User ID:** {banned_user_id}\n**Reason:** ```{reason}```",
-                    )),
-                    None =>                     embed = embed.description(format!(
-                        "**User:** <@{banned_user_id}> - {banned_user_name}\n**User ID:** {banned_user_id}",
-                    )),
-                };
+            embed = embed.field(EmbedFieldBuilder::new("User", format!("<@{banned_user_id} - {banned_user_name}>")).inline());
+            embed = embed.field(EmbedFieldBuilder::new("User ID", banned_user_id.to_string()).inline());
+            embed = embed.field(EmbedFieldBuilder::new("Guild ID", guild.id.to_string()).inline());
+
+            if let Some(reason) = resolved_ban.reason {
+                embed = embed.field(EmbedFieldBuilder::new("User", format!("```{reason}```")));
+            }            
 
             self.twilight_client
                 .create_message(moderator_actions_log_channel)
