c36033a7bf7b3445fe13b66e8a3ef85378584f0b
Aaron Sheppard <nurah@wolfo.tech>
2023-07-31 00:14:05 +0100

    FEAT: Remove logging message content to console

--- src/event_handler/message_create.rs
+++ src/event_handler/message_create.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use anyhow::Error;
-use tracing::info;
 use twilight_model::gateway::payload::incoming::MessageCreate;
 
 use crate::models::{LuroFramework, UserData};
@@ -13,13 +12,6 @@ impl LuroFramework {
             let lowercase = message.content.to_ascii_lowercase();
             let words: Vec<&str> = lowercase.split_whitespace().collect();
             UserData::write_words(&self, words, &message.author.id).await?;
-        }
-
-        if message.author.id != self.twilight_client.current_user().await?.model().await?.id
-            || !message.author.bot
-            || !message.content.is_empty()
-        {
-            info!("Message Received - Author: {}\n{}", message.author.name, message.0.content);
         }
 
         Ok(())
