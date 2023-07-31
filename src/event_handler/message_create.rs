608b5966d48a16f6b0d44943fb6a6c0b06931166
Aaron Sheppard <nurah@wolfo.tech>
2023-07-31 15:59:27 +0100

    Let message handler catch embed text

--- src/event_handler/message_create.rs
+++ src/event_handler/message_create.rs
@@ -6,7 +6,13 @@ use twilight_model::gateway::payload::incoming::MessageCreate;
 use crate::models::{LuroFramework, UserData};
 
 impl LuroFramework {
-    pub async fn message_create_listener(self: Arc<Self>, message: Box<MessageCreate>) -> Result<(), Error> {
+    pub async fn message_create_listener(self: Arc<Self>, mut message: Box<MessageCreate>) -> Result<(), Error> {
+        for embed in message.embeds.clone() {
+            if let Some(ref description) = embed.description {
+                message.content.push_str(description)
+            }
+        }
+
         if !message.content.is_empty() {
             UserData::write_words(&self, &message.content, &message.author.id).await?;
         }
