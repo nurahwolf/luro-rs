5d431148bd7db36b297482b9154867ab7306f85c
Aaron Sheppard <nurah@wolfo.tech>
2023-08-02 13:12:56 +0100

    Fix saving bug + optional ID to remove

--- src/commands/owner/clear_warnings.rs
+++ src/commands/owner/clear_warnings.rs
@@ -16,42 +16,47 @@ use crate::traits::toml::LuroTOML;
 pub struct OwnerClearWarning {
     /// The user to clear
     pub user: ResolvedUser,
-    /// The warning ID to remove
-    pub id: i64
+    /// The warning ID to remove. Removes all if not set.
+    pub id: Option<i64>
 }
 
 #[async_trait]
 impl LuroCommand for OwnerClearWarning {
     async fn run_command(self, ctx: LuroSlash) -> anyhow::Result<()> {
-        let path = format!("{0}/{1}/user_settings.toml", USERDATA_FILE_PATH, self.id);
+        let path = format!("{0}/{1}/user_settings.toml", USERDATA_FILE_PATH, self.user.resolved.id);
         let path = Path::new(&path);
         let mut user_data = UserData::get_user_settings(&ctx.luro, &self.user.resolved.id).await?;
         match user_data.warnings.as_mut() {
             Some(warnings) => {
-                let index: usize = match usize::try_from(self.id) {
-                    Ok(index) => match index.checked_sub(1) {
-                        Some(index) => index,
-                        None => {
-                            return ctx
-                                .content("This function automatically reduces the ID by 1. You just had the buffer underflow")
-                                .respond()
-                                .await
-                        }
-                    },
-                    Err(_) => return ctx.content("Failed to convert ID to usize").respond().await
-                };
+                if let Some(index) = self.id {
+                    let index: usize =
+                        match usize::try_from(index) {
+                            Ok(index) => match index.checked_sub(1) {
+                                Some(index) => index,
+                                None => return ctx
+                                    .content(
+                                        "This function automatically reduces the ID by 1. You just had the buffer underflow"
+                                    )
+                                    .respond()
+                                    .await
+                            },
+                            Err(_) => return ctx.content("Failed to convert ID to usize").respond().await
+                        };
 
-                if index > warnings.len() || warnings.is_empty() {
-                    return ctx
-                        .content(format!(
-                            "The vector has {} elements. You are trying to remove a number greater than that.",
-                            warnings.len()
-                        ))
-                        .respond()
-                        .await;
+                    if index > warnings.len() || warnings.is_empty() {
+                        return ctx
+                            .content(format!(
+                                "The vector has {} elements. You are trying to remove a number greater than that.",
+                                warnings.len()
+                            ))
+                            .respond()
+                            .await;
+                    }
+                    warnings.remove(index);
+                } else {
+                    // Drain all warnings
+                    warnings.drain(..);
                 }
-
-                warnings.remove(index);
             }
             None => return ctx.content("User has no warnings you stupid idiot!").respond().await
         }
