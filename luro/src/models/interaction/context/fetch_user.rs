c88c4e34af590fbe2377fa5597946d37b70fbd54
Nurah <nurah@wolfo.tech>
2024-03-16 01:39:24 +0000

    FIX: Allow fetch user to fall back to user context

--- luro/src/models/interaction/context/fetch_user.rs
+++ luro/src/models/interaction/context/fetch_user.rs
@@ -6,7 +6,13 @@ impl super::InteractionContext {
     /// Fetch a user, getting member data if the interaction is in a guild
     pub async fn fetch_user(&self, user_id: Id<UserMarker>) -> Result<User, InteractionError> {
         match self.interaction.guild_id {
-            Some(guild_id) => self.gateway.database.fetch_member(guild_id, user_id).await.map(|x|User::Member(x)),
+            Some(guild_id) => match self.gateway.database.fetch_member(guild_id, user_id).await {
+                Ok(member) => Ok(User::Member(member)),
+                Err(why) => {
+                    tracing::error!(?why, "Failed to fetch member, falling back to user...");
+                    self.gateway.database.fetch_user(user_id).await
+                }
+            },
             None => self.gateway.database.fetch_user(user_id).await,
         }
     }
