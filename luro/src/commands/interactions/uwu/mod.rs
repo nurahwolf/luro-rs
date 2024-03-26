81e430de9e2ec2046be774cae52db6af7aecb649
Nurah <nurah@wolfo.tech>
2024-03-26 13:10:22 +0000

    CHORE: Unmark uwuify as unsafe

--- luro/src/commands/interactions/uwu/mod.rs
+++ luro/src/commands/interactions/uwu/mod.rs
@@ -12,7 +12,7 @@ pub struct UwU {
 impl crate::models::CreateCommand for UwU {
     async fn handle_command(self, framework: &mut InteractionContext) -> InteractionResult<()> {
         #[cfg(target_feature = "sse4.1")]
-        let uwu = unsafe { uwuifier::uwuify_str_sse(&self._message) };
+        let uwu = uwuifier::uwuify_str_sse(&self._message);
 
         #[cfg(not(target_feature = "sse4.1"))]
         let uwu = "Sorry, UwU is not possible on ARM currently :(";
